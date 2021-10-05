d7f91b4488c282dfe6f8eb17894dce235af3bb93
David Welander <welander@adobe.com>
2021-10-05 15:20:25 +0200

    added click

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { useEffect, useState} from 'react';
-import Slider from 'rc-slider';
+import React, { Component, useEffect, useState} from 'react';
+// import Slider from 'rc-slider';
+import Click from "./Click";
 import API, { graphqlOperation } from '@aws-amplify/api'
 
 import * as subscriptions from './graphql/subscriptions'
@@ -12,60 +13,12 @@ import awsconfig from './aws-exports';
 
 API.configure(awsconfig)
 
+export default class App extends Component{
+    render = () =>
+        <div className="App">
+            <div>
+                <Click name="myPage"/>
+            </div>
 
-const Counter = (props) => {
-
-  const [value, setValue] = useState(0)
-
-  useEffect(() => {
-    let subscription
-
-    async function setupSubscription() {
-      subscription = API.graphql({
-          query: subscriptions.counterChange,
-          variables: { name: props.name },
-          authMode: 'API_KEY',
-        }).subscribe({
-        next: (data) => {
-          const counterChange = data.value.data.counterChange
-          setValue(counterChange.value)
-        }
-      })
-    }
-
-    setupSubscription()
-
-    return () => subscription.unsubscribe();
-  }, [props.name])  
-
-  const sendUpdate = async (e) => {
-    await API.graphql(graphqlOperation(mutations.setCounter, { name: props.name, value: e.target.value}))
-  }
-
-  return (
-    <input type="range" min="0" max="100" value={value} onChange={ (e) => setValue(e.target.value) } onMouseUp={sendUpdate} />
-  )
+        </div>
 }
-
-
-
-function App() {
-
-  return (
-    <div className="App">
-      <h1>Multiplayer Realtime Slider Demo</h1>
-
-      <p>Open me on multiple browsers, then change a slider in one browser and watch the same one update on all the other browsers.</p>
-
-      <div>
-        Slider 1:  <Counter name="Slider1" />
-      </div>
-
-      <div>
-        Slider 2: <Counter name="Slider2" />
-      </div>
-    </div>
-  );
-}
-
-export default App;
--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -2,7 +2,7 @@
 // this is an auto generated file. This will be overwritten
 
 export const setCounter = /* GraphQL */ `
-  mutation SetCounter($name: String!, $value: Int!) {
+  mutation SetCounter($name: String!, $value: String!) {
     setCounter(name: $name, value: $value) {
       value
       name
